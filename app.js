3a7e105354d16ca2cae01f07af74d4b80b1bee1f
neha <sky32neha@gmail.com>
2021-01-08 15:34:03 +0530

    all are added successfully

---
+++ app.js
@@ -0,0 +1,7 @@
+var users = {
+    name : "neha",
+    age : 20,
+    skill : ["java","sql",],
+};
+let JSONData=JSON.stringify(users)
+console.log (JSONData);
---
+++ app2.js
@@ -0,0 +1,8 @@
+var users = {
+    name : "neha",
+    age : 20,
+    skill : ["java","sql"]
+};
+console.log(users.age)
+console.log(users.skill)
+
